8ad4f3de6c95b95f112284824458c56e640cf3b7
Ahmed Noukta <ahmed.noukta@gmail.com>
2025-03-17 11:23:49 +0000

    Fix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
             <div className="flex items-center">
-              <img src="/assets/logo.png" alt="Logo" className="h-10 w-32" />
+              <img src="/assets/Logo.png" alt="Logo" className="h-10 w-32" />
             </div>
           
             <div className="hidden md:flex items-center space-x-8">
